eb1494f2a1df8a346c1476260176b74b8a01605b
lmdoyle <leodoyle05@gmail.com>
2026-06-11 14:45:30 -0400

    Initial commit: Cuphead progress overlay and admin

---
+++ config.js
@@ -0,0 +1,10 @@
+// Replace with your Firebase project credentials
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
